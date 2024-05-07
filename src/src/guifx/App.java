7f02fb1c46f9efa058bce31fe48e4244b774271c
amel100k <eaa23ema@students.eaaa.dk>
2024-05-07 09:35:33 +0200

    hh

--- src/src/guifx/App.java
+++ src/src/guifx/App.java
@@ -1,4 +1,10 @@
 package guifx;
 
+import application.model.Fad;
+import application.model.Lager;
+
 public class App {
+    public static void main(String[] args) {
+
+    }
 }
--- src/src/storage/Storage.java
+++ src/src/storage/Storage.java
@@ -1,4 +1,12 @@
 package storage;
 
+import application.model.Destillering;
+import application.model.Fad;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Storage {
+    private static ArrayList<Fad> fad = new ArrayList<>();
+    private static ArrayList<Destillering> destilleringer = new ArrayList<>();
 }
