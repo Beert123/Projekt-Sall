e1c1e32c8b00045c142e9b3a3765e5ffee9f1e26
Beert123 <bertil@selmas.dk>
2024-05-07 08:53:12 +0200

    Destillering klasse oprettet

--- src/src/application/model/Fad.java
+++ src/src/application/model/Fad.java
@@ -6,4 +6,12 @@ public class Fad {
     private int placering;
     private String koebssted;
     private String fadNavn;
+
+    public Fad(String fadHistore, String tidligereBrug, int placering, String koebssted, String fadNavn) {
+        this.fadHistore = fadHistore;
+        this.tidligereBrug = tidligereBrug;
+        this.placering = placering;
+        this.koebssted = koebssted;
+        this.fadNavn = fadNavn;
+    }
 }
