732296a454653e0742d395688133d9d6911b2401
lucas <minibba@protonmail.com>
2024-05-08 09:25:14 +0200

    opdateret storage

--- src/src/storage/Storage.java
+++ src/src/storage/Storage.java
@@ -4,20 +4,22 @@ import application.model.Destillat;
 import application.model.Destillering;
 import application.model.Fad;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
 public class Storage {
-    private static ArrayList<Fad> fad = new ArrayList<>();
+    private static ArrayList<Fad> fade = new ArrayList<>();
     private static ArrayList<Destillering> destilleringer = new ArrayList<>();
     private static ArrayList<Destillat> destillater = new ArrayList<>();
     public static void addDestillering(Destillering destillering) {
         destilleringer.add(destillering);
     }
+    public static void addFad(Fad fad) {
+        fade.add(fad);
+    }
 
-    public static ArrayList<Fad> getFad() {
-        return fad;
+    public static ArrayList<Fad> getFade() {
+        return fade;
     }
 
     public static ArrayList<Destillering> getDestilleringer() {
