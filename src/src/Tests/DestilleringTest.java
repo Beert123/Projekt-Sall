34e6de48fc8b53fa534750da45a34ccb026e921a
lucas <minibba@protonmail.com>
2024-05-13 09:20:38 +0200

    fadtest

--- src/src/Tests/DestilleringTest.java
+++ src/src/Tests/DestilleringTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DestilleringTest {
 
     @Test
-    void shouldAddNewDestillatWhenNotAlreadyPresent() {
+    void addDestillat() {
         Destillering destillering = new Destillering(LocalDate.of(2024,2,2), LocalDate.of(2024,3,2), 1000,"Byg", 800,84.9,"Ingen kommentar", "Tørv");
         Destillat destillat = new Destillat(60.7,new Mængde(102));
         destillering.addDestillat(destillat);
@@ -20,7 +20,7 @@ class DestilleringTest {
     }
 
     @Test
-    void shouldNotAddExistingDestillat() {
+    void addAlleredeEksisterendeDestillat() {
         Destillering destillering = new Destillering(LocalDate.of(2024,4,2), LocalDate.of(2024,5,2), 500,"Byg", 750,87.4,"Ingen kommentar", "Ingen");
         Destillat destillat = new Destillat(60.2,new Mængde(122));
         destillering.addDestillat(destillat);
@@ -31,7 +31,7 @@ class DestilleringTest {
     }
 
     @Test
-    void shouldCreateAndAddNewDestillat() {
+    void createOgAddDestillat() {
         Destillering destillering = new Destillering(LocalDate.of(2024,3,2), LocalDate.of(2024,4,2), 1000,"Byg", 800,85.6,"Ingen kommentar", "Ingen");
         int sizeBefore = destillering.getDestillater().size();
         Destillat destillat = destillering.createDestillat(40.0, new Mængde(132));
