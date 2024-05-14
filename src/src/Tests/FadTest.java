b467dd33b66dcacc4f7460ea5acbf184a514745d
lucas <minibba@protonmail.com>
2024-05-14 09:32:13 +0200

    exception

--- src/src/Tests/FadTest.java
+++ src/src/Tests/FadTest.java
@@ -7,27 +7,26 @@ class FadTest {
 
     @Test
     void tilføjVedPladsTilgængelig() {
-        Fad fad = new Fad("Benns kælder", "Sherry", 1, "Spanien", "Ola", 100);
+        Fad fad = new Fad("Benns kælder", "Sherry", 1, "Spanien", "S1", 100);
         fad.fyldPåFad(50);
         assertEquals(50, fad.getMængdePåFad());
     }
 
     @Test
     void påfyldningAfAlleredeFyldtFad() {
-        Fad fad = new Fad("Bertils kælder", "Bourbon", 1, "Danmark", "B0", 100);
-        fad.fyldPåFad(101);
+        Fad fad = new Fad("Bertils kælder", "Bourbon", 1, "Danmark", "B1", 100);
         assertThrows(RuntimeException.class, () -> fad.fyldPåFad(101));    }
 
     @Test
     void påfyldningTopGrænseværdiAfFad() {
-        Fad fad = new Fad("Emils kælder", "Pisang ambon", 1, "Skotland", "Jul", 100);
+        Fad fad = new Fad("Emils kælder", "Pisang ambon", 1, "Skotland", "PA1", 100);
         fad.fyldPåFad(100);
         assertEquals(100, fad.getMængdePåFad());
     }
 
     @Test
     void tilføjningAfNul() {
-        Fad fad = new Fad("Lucas' kælder", "Ubrugt", 1, "USA", "Thy", 100);
+        Fad fad = new Fad("Lucas' kælder", "Ubrugt", 1, "USA", "U1", 100);
         fad.fyldPåFad(0);
         assertEquals(0, fad.getMængdePåFad());
     }
