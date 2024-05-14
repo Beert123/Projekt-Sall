d8d37207c6e8e0fed2aeb0fa3973042bad41d6d6
Beert123 <bertil@selmas.dk>
2024-05-14 09:41:48 +0200

    Startet på aftapningPane

--- src/src/application/model/Aftapning.java
+++ src/src/application/model/Aftapning.java
@@ -6,24 +6,26 @@ import java.util.ArrayList;
 public class Aftapning {
     private Fad fad;
     private ArrayList<Destillat> destillat;
-    private int literDerFyldesPåFad;
+    private int liter;
     private LocalDate dato;
 
     public Aftapning(Fad fad, ArrayList<Destillat> destillat, int literDerFyldesPåFad, LocalDate dato) {
         this.fad = fad;
         this.destillat = destillat;
-        this.literDerFyldesPåFad = literDerFyldesPåFad;
+        this.liter = literDerFyldesPåFad;
         this.dato = dato;
     }
 
-
+    public int getLiter() {
+        return liter;
+    }
 
     @Override
     public String toString() {
         return "Aftapning{" +
                 "fad=" + fad +
                 ", destillat=" + destillat +
-                ", literDerFyldesPåFad=" + literDerFyldesPåFad +
+                ", literDerFyldesPåFad=" + liter +
                 ", dato=" + dato +
                 '}';
     }
